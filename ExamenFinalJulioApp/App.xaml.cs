92f26c3062c97339afabf6054f871d30b04cf0ad
Julio César Solís Jímenez <julio20092003@hotmail.com>
2024-08-21 00:32:21 -0600

    Add project files.

---
+++ ExamenFinalJulioApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamenFinalJulioApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ExamenFinalJulioApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExamenFinalJulioApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
